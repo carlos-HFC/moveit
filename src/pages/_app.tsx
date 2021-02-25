c6996a97d11bf51ae0249c765d4f3c73f3ff43ec
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-25 09:44:38 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import { ChallengeContext, ChallengeProvider } from '../context/ChallengeContext'
+import { ChallengeProvider } from '../context/ChallengeContext'
 
 import '../css/global.min.css'
 
 function MyApp({ Component, pageProps }) {
-  const [level, setLevel] = useState(1)
-
-  function levelUp() {
-    setLevel(level + 1)
-  }
-
   return (
     <ChallengeProvider>
       <Component {...pageProps} />
