25748840d37db1ee844d1ac1a394a8739329308b
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-25 09:43:19 -0300

    Passar funções e variáveis para o contexto

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,43 +1,12 @@
 import { useContext, useEffect, useState } from 'react'
-import { ChallengeContext } from '../context/ChallengeContext'
-
-let countdownTimeout: NodeJS.Timeout
+import { CountdownContext } from '../context/CountdownContext'
 
 export const Countdown = () => {
-  const { startNewChallenge } = useContext(ChallengeContext)
-
-  const [isActive, setIsActive] = useState(false)
-  const [hasFinished, setHasFinished] = useState(false)
-  const [time, setTime] = useState(25 * 60)
-
-  const minutes = Math.floor(time / 60)
-  const seconds = time % 60
+  const { minutes, seconds, isActive, hasFinished, startCountdown, resetCountdown } = useContext(CountdownContext)
 
   // padStart(length, 'string') faz com que a string tenha um tamanho informado e inicie com a string passada. No caso, a string terá 2 caracteres e iniciará com zero
   const [minLeft, minRight] = String(minutes).padStart(2, '0').split('')
   const [secLeft, secRight] = String(seconds).padStart(2, '0').split('')
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1)
-      }, 1000)
-    } else if (isActive && time === 0) {
-      setHasFinished(true)
-      setIsActive(false)
-      startNewChallenge()
-    }
-  }, [isActive, time])
-
-  function startCountdown() {
-    setIsActive(true)
-  }
-
-  function resetCountdown() {
-    clearTimeout(countdownTimeout)
-    setIsActive(false)
-    setTime(25 * 60)
-  }
 
   return (
     <>
