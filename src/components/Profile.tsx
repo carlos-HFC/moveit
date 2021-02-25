289d6773b8f1f17aef282d3d9949b5c1997e96bf
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-25 09:43:35 -0300

    Atualizar o level a partir do contexto

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useContext } from 'react'
+import { ChallengeContext } from '../context/ChallengeContext'
 
 export const Profile = () => {
+  const { level } = useContext(ChallengeContext)
+
   return (
     <div className="profile__container">
       <img src="https://github.com/carlos-hfc.png" alt="Carlos Faustino" />
@@ -8,7 +11,7 @@ export const Profile = () => {
         <strong>Carlos Faustino</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
