01d25f5bea8213db23e4f2ea24c5197a22e1368a
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-26 15:26:23 -0300

    Inserção de cookies e SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,58 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { ChallengeBox, CompletedChallenge, Countdown, ExperienceBar, Profile } from "../components";
+import { ChallengeProvider } from "../context/ChallengeContext";
 import { CountdownProvider } from "../context/CountdownContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className="container">
-      <Head>
-        <title>Início | Move.it</title>
-      </Head>
+    <ChallengeProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className="container">
+        <Head>
+          <title>Início | Move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenge />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenge />
+              <Countdown />
+            </div>
+
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengeProvider>
   )
+}
+
+/**
+ * DEVE SER OBRIGATORIAMENTE ESTE NOME E PRECISA SER ASSÍNCRONA
+ */
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
